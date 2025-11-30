0b24779db16fe4833c64ee0b55b78a69c6933578
Tarun Birla <tarunbirla2018@gmail.com>
2025-11-30 12:31:22 +0530

    Your commit home responsivdvzdghvdkjhjhdfc

--- src/components/Abouts.jsx
+++ src/components/Abouts.jsx
@@ -1,10 +1,30 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import http from "../service/http";
 
 
 const Abouts = () => {
+    const [abouts, setAbouts] = useState([]);
+    const [aboutsdeco, setAboutsdeco] = useState([]);
+  
+    const fetchData = async () => {
+      try {
+        const response = await http.get(`/common`);
+        console.log("Fetched data:", response.data);
+        const Alldata = response.data?.data;
+        setAbouts(Alldata?.chairmanmsg[0]);
+        setAboutsdeco(Alldata?.lastdecade[0]);
+        
+      } catch (err) {
+        console.error("Error fetching commen data:", err);
+      }
+    };
+  
+    useEffect(() => {
+      fetchData();
+    }, []);
     return (
         <>
       <Header />
@@ -31,7 +51,7 @@ const Abouts = () => {
   {/* LEFT SIDE IMAGE WITH BLUE BACKGROUND */}
   <div className="md:w-1/2 bg-blue-700 flex items-center justify-center p-0 md:h-[550px]">
     <img
-      src="/img/mohammad.png"
+      src={abouts?.image_url || "/img/chairman.png"}
       alt="Mohammad Jamaluddin"
       className="w-full h-full "
     />
@@ -44,25 +64,19 @@ const Abouts = () => {
     </p>
 
     <h2 className="text-3xl md:text-4xl font-bold mb-6">
-      Mohammad Jamaluddin
+     {abouts?.title}
     </h2>
 
-    <p className="text-gray-700 leading-relaxed mb-6">
-      As Chairman at Premier Group, Mr. Mohammad Jamaluddin strategically
-      drives growth and cultivates key partnerships across its diverse
-      operations, including property development and integrated townships.
-      His focus on business development and market penetration is instrumental
-      in expanding the Group's impact in these crucial sectors. Committed to
-      Premier Group’s “Growth with Purpose” ethos, Mr. Mohammad Jamaluddin
-      plays a key role in realising the vision of developing thriving and
-      sustainable communities.
+    <p className="text-gray-700 leading-relaxed mb-6"
+    dangerouslySetInnerHTML={{ __html: abouts?.description }}>
+     
     </p>
 
-    <p className="text-gray-900 font-semibold italic leading-relaxed">
+    {/* <p className="text-gray-900 font-semibold italic leading-relaxed">
       “I have never looked at the balance sheet of any company I took over.
       It was pure gut-feel and I never went wrong. The moment I ignored the
       gut reaction, I made a mistake.”
-    </p>
+    </p> */}
   </div>
 
 </section>
@@ -73,35 +87,18 @@ const Abouts = () => {
       {/* Heading */}
       <p className="text-[#98C20B] font-semibold tracking-wide">GROWTH</p>
       <h2 className="text-4xl font-bold mt-2 mb-12">
-        Growth in the Last Decade
+        {aboutsdeco?.title}
       </h2>
 
       {/* Two Column Layout */}
       <div className="grid md:grid-cols-2 gap-2">
         
         {/* LEFT BLUE BOX */}
-        <div className="bg-[#0B5394] text-white p-8 md:p-12 rounded-md flex flex-col justify-center">
-          <h3 className="text-2xl md:text-3xl font-bold mb-6">
-            Commitment to Purposeful Growth
-          </h3>
+        <div className="bg-[#0B5394] text-white p-8 md:p-12 rounded-md flex flex-col justify-center"
+        dangerouslySetInnerHTML={{ __html: aboutsdeco?.description }}>
+       
 
-          <p className="leading-relaxed mb-4">
-            From its beginnings in global apparel manufacturing to its diversified 
-            ventures across industries, <span className="font-semibold">Premier Group</span> has remained committed 
-            to combining <span className="font-semibold">innovation, integrity, and impact.</span>
-          </p>
-
-          <p className="leading-relaxed mb-4">
-            Under the visionary leadership of 
-            <span className="font-semibold"> Chairman Mohammad Jamaluddin</span>, the Group continues 
-            to invest in initiatives that 
-            <span className="font-semibold"> create economic opportunity, promote education,</span> 
-            and uplift communities — staying true to its enduring principle:
-          </p>
-
-          <p className="italic font-semibold text-lg">
-            “Growth with Purpose.”
-          </p>
+         
         </div>
 
         {/* RIGHT IMAGE */}
