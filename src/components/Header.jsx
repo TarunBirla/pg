50cf56eb68ec4655f30b30a60bbc5af7d949c4cf
infoharry99 <infoharry99@gmail.com>
2026-02-25 21:59:10 +0530

    25-02

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -128,9 +128,9 @@ const Header = () => {
                 >
             {/* TOP BAR */}
             <div className="flex items-center justify-between px-6 py-4 border-b border-gray-200">
-              <span className="h-10 w-auto object-contain">
+              {/* <span className="h-10 w-auto object-contain">
                 Menu
-              </span>
+              </span> */}
 
                 
               <button onClick={() => setMenuOpen(false)}>
